69597188a634b2d5a301258ed1a630a33af0d551
bijay-05 <hawkeyesniper69@gmail.com>
2026-01-21 17:17:35 +0545

    add getCategoryList funuction and api

---
+++ category/categoryList.js
@@ -0,0 +1,47 @@
+const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+
+const {
+  DynamoDBDocumentClient,
+  ScanCommand,
+} = require("@aws-sdk/lib-dynamodb");
+
+const CATEGORY_TABLE = process.env.CATEGORY_TABLE;
+const client = new DynamoDBClient();
+const docClient = DynamoDBDocumentClient.from(client);
+
+exports.getCategoryList = async () => {
+  const params = {
+    TableName: CATEGORY_TABLE,
+  };
+
+  try {
+    const command = new ScanCommand(params);
+    const { Items } = await docClient.send(command);
+
+    if (Items.length == 0) {
+      return {
+        statusCode: 404,
+        body: JSON.stringify({
+          message: "Not found",
+          data: [],
+        }),
+      };
+    } else {
+      return {
+        statusCode: 200,
+        body: JSON.stringify({
+          message: "Category list retrieved successfully!",
+          data: Items,
+        }),
+      };
+    }
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: "Error creating category",
+      }),
+    };
+  }
+};
